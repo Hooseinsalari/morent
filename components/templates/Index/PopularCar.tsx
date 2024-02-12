8663bbe76882733805161f9eb0a0289a4f30de9a
Hossein Salari <hossein.salari002@gmail.com>
2024-02-12 23:32:27 +0330

    basic folder structure

---
+++ components/templates/Index/PopularCar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PopularCar = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default PopularCar;
---
+++ components/templates/Index/RecomendationCar.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const RecomendationCar = () => {
+  return <div></div>;
+};
+
+export default RecomendationCar;
