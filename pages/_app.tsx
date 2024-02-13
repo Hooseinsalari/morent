0fe158bf3562d49903c930fb4371433c29b06696
Hossein Salari <hossein.salari002@gmail.com>
2024-02-13 20:30:18 +0330

    feat(Font): add font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,21 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Plus_Jakarta_Sans } from "next/font/google";
 
 // components
 import Navbar from "@/components/modules/Navbar/Navbar";
 
+const plus = Plus_Jakarta_Sans({
+  subsets: ["latin"],
+  weight: ["200", "300", "400", "500", "600", "700", "800"],
+  display: "swap",
+});
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <div className={plus.className}>
       <Navbar />
       <Component {...pageProps} />
-    </>
+    </div>
   );
 }
