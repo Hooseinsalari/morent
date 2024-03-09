da6322b720ee5db4f975bcc8f0ddfc0431734bf6
Hossein Salari <hossein.salari002@gmail.com>
2024-03-09 19:45:33 +0330

    feat: implement sign out api

--- pages/api/auth/signOut.ts
+++ pages/api/auth/signOut.ts
@@ -1,8 +1,30 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse,
-) {
-  res.status(200).json({ message: "Hello" });
+// cookie
+import { serialize } from "cookie";
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method !== "GET") {
+    return false;
+  }
+
+  try {
+    return res
+      .setHeader(
+        "Set-Cookie",
+        serialize("token", "", {
+          maxAge: 0,
+          path: "/",
+        })
+      )
+      .status(200)
+      .json({
+        message: "Logout successful. We hope to see you again soon!",
+        status: "success",
+      });
+  } catch (error) {
+    return res.status(500).json({
+      message: "An unexpected error occurred. Please try again later.",
+    });
+  }
 }
