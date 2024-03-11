dd07ef2a89158483344a97f3fcc8b81f504c26b1
Hossein Salari <hossein.salari002@gmail.com>
2024-03-11 16:17:59 +0330

    feat: implement dynamic page for each car and get data

---
+++ pages/vehicles/[id]/index.tsx
@@ -0,0 +1,41 @@
+import { CarInterface } from "@/types";
+import axios from "axios";
+import { GetStaticPaths, GetStaticProps } from "next";
+import React from "react";
+
+const index = () => {
+  return <div className="px-6 md:px-16 py-8">salam</div>;
+};
+
+export default index;
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const { data } = await axios.get("http://localhost:3000/api/cars");
+  const paths = data.data.map((car: CarInterface) => {
+    return {
+      params: { id: car._id },
+    };
+  });
+
+  return { paths, fallback: false };
+};
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const { params } = context;
+
+  const { data } = await axios.get(
+    `http://localhost:3000/api/cars/${params?.id}`
+  );
+
+  if (data.data) {
+    return {
+      props: {
+        car: data.data,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+};
