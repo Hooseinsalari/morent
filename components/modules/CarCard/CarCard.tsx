a0f3526529ce8bcc75da60380a2e5aff096dd5b4
Hossein Salari <hossein.salari002@gmail.com>
2024-03-15 23:52:38 +0330

    feat: add rent car handler with context

--- components/modules/CarCard/CarCard.tsx
+++ components/modules/CarCard/CarCard.tsx
@@ -1,3 +1,4 @@
+import { useRouter } from "next/navigation";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
@@ -10,7 +11,31 @@ import GasStation from "@/public/svg/gas-station.svg";
 import Gear from "@/public/svg/gear.svg";
 import Capacity from "@/public/svg/capacity.svg";
 
+// context
+import { useRentalCart } from "@/context/RentalCartContextProvider";
+import { useUser } from "@/context/UserContextProvider";
+
+// toast
+import toast from "react-hot-toast";
+
 const CarCard = ({ car }: { car: CarInterface }) => {
+  // ** context
+  const { dispatch } = useRentalCart();
+  const { user } = useUser();
+
+  // ** router
+  const router = useRouter();
+
+  // ** handler
+  const rentCarHandler = () => {
+    if (user) {
+      dispatch({ type: "RENT", payload: car });
+    } else {
+      toast.error("Login required to proceed with car rental.");
+      router.push("/signin?redirect=payment");
+    }
+  };
+
   return (
     <div className="bg-white rounded-lg px-3 py-4 mt-5 w-full">
       <div className="mb-4">
@@ -79,7 +104,10 @@ const CarCard = ({ car }: { car: CarInterface }) => {
           ${car.price.toFixed(2)}/
           <span className="text-xs text-secondinary-300">day</span>
         </h3>
-        <button className="bg-primary-500 text-sm text-white px-4 py-2 rounded-[4px]">
+        <button
+          onClick={rentCarHandler}
+          className="bg-primary-500 text-sm text-white px-4 py-2 rounded-[4px]"
+        >
           Rental Now
         </button>
       </div>
