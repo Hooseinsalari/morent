931b5b121613f3dc8eda124af318366589cc2001
Hossein Salari <hossein.salari002@gmail.com>
2024-03-24 16:36:10 +0330

    feat: implement Sidebar in dashboard page

---
+++ components/templates/Dashboard/Sidebar.tsx
@@ -0,0 +1,131 @@
+import React, { useState } from "react";
+import axios from "axios";
+import Link from "next/link";
+import Image from "next/image";
+
+// context
+import { useUser } from "@/context/UserContextProvider";
+
+// toast
+import toast from "react-hot-toast";
+
+// svg
+import Menu from "@/public/svg/menu.svg";
+import Logout from "@/public/svg/logout2.svg";
+
+// constant
+import { mainMenuItems, preferencesItems } from "@/constant";
+
+const Sidebar = () => {
+  // ** states
+  const [isOpen, setIsOpen] = useState(false);
+
+  // ** context
+  const { setUser } = useUser();
+
+  // ** handler
+  const logoutHandler = async () => {
+    const response = await axios.get("http://localhost:3000/api/auth/signOut");
+    if (response.status === 200) {
+      toast.success(response.data.message);
+      setUser(null);
+    } else {
+      toast.error("An unexpected error occurred. Please try again later.");
+    }
+  };
+
+  return (
+    <>
+      <div className="w-full lg:w-[30%] xl:w-[25%] z-10 h-auto">
+        <div
+          className={`bg-white rounded-br-[5rem] lg:rounded-none p-6 pr-1 w-3/4 sm:w-1/2 absolute top-0 left-0 z-30 lg:translate-x-0 lg:sticky lg:w-full ${
+            isOpen ? "translate-x-0" : "translate-x-[-100%]"
+          } transition-transform duration-300 ease-in-out`}
+        >
+          <button
+            onClick={() => setIsOpen((prevState) => !prevState)}
+            className={`lg:hidden absolute -z-10 -top-0 -right-12 bg-white border border-l-0 p-2 pl-10 rounded-full cursor-pointer ${
+              isOpen && "border-0 pl-2"
+            }`}
+          >
+            <Image className="w-8 h-8" src={Menu} alt="menu" />
+          </button>
+          <div className="h-screen flex flex-col">
+            <div className="flex-grow overflow-y-auto">
+              <div className="w-full">
+                <h1 className="text-secondinary-200 text-xs mb-5 tracking-wider ml-4">
+                  MANIN MENU
+                </h1>
+                <ul>
+                  {mainMenuItems.map((i) => (
+                    <Link
+                      href="/dashboard"
+                      key={i.item}
+                      className="flex items-center p-4 hover:bg-primary-500 duration-300 hover:fill-white rounded-xl text-secondinary-400 font-semibold hover:text-white mb-2"
+                    >
+                      <Image
+                        width={22}
+                        height={22}
+                        src={i.icon}
+                        alt={i.item}
+                        className="mr-3"
+                      />
+                      <span>{i.item}</span>
+                    </Link>
+                  ))}
+                </ul>
+              </div>
+
+              <div className="mt-16 w-full">
+                <h1 className="text-secondinary-200 text-xs mb-5 tracking-wider ml-4">
+                  PREFERENCES
+                </h1>
+                <ul>
+                  {preferencesItems.map((i) => (
+                    <Link
+                      href="/dashboard"
+                      key={i.item}
+                      className="flex items-center p-4 hover:bg-primary-500 duration-300 hover:fill-white rounded-xl text-secondinary-400 font-semibold hover:text-white mb-2"
+                    >
+                      <Image
+                        width={22}
+                        height={22}
+                        src={i.icon}
+                        alt={i.item}
+                        className="mr-3"
+                      />
+                      <span>{i.item}</span>
+                    </Link>
+                  ))}
+                </ul>
+              </div>
+            </div>
+
+            <div className="w-full my-8 flex-1">
+              <button
+                onClick={logoutHandler}
+                className="flex w-full items-center p-4 hover:bg-primary-500 duration-300 hover:fill-white rounded-xl text-secondinary-400 font-semibold hover:text-white mb-2"
+              >
+                <Image
+                  width={22}
+                  height={22}
+                  src={Logout}
+                  alt="logout"
+                  className="mr-3"
+                />
+                <span>Logout</span>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div
+        className={`lg:hidden w-full absolute top-0 left-0 right-0 bg-[rgba(0,0,0,0.5)] h-full ${
+          isOpen ? "block" : "hidden"
+        }`}
+      ></div>
+    </>
+  );
+};
+
+export default Sidebar;
--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,5 +1,6 @@
 import DetailRental from "@/components/templates/Dashboard/DetailRental";
 import RecentTransaction from "@/components/templates/Dashboard/RecentTransaction";
+import Sidebar from "@/components/templates/Dashboard/Sidebar";
 import TopRentedCar from "@/components/templates/Dashboard/TopRentedCar";
 import { useUser } from "@/context/UserContextProvider";
 import axios from "axios";
@@ -18,15 +19,19 @@ const Dashboard = () => {
   const lastCar: number = user?.rentedCars ? user?.rentedCars?.length - 1 : 0;
 
   return (
-    <div className="lg:flex lg:justify-start lg:gap-x-8 lg:p-8 lg:w-full">
-      <DetailRental />
+    <div className="px-6 py-8 relative lg:static lg:flex lg:flex-row-reverse lg:justify-between lg:p-0 lg:gap-4">
+      <div className="lg:flex lg:justify-start lg:gap-x-8 lg:p-8 lg:w-full">
+        <DetailRental />
 
-      <div
-        className={`${user?.rentedCars[lastCar] ? "lg:w-1/2" : "lg:w-full"}`}
-      >
-        <TopRentedCar />
-        <RecentTransaction />
+        <div
+          className={`${user?.rentedCars[lastCar] ? "lg:w-1/2" : "lg:w-full"}`}
+        >
+          <TopRentedCar />
+          <RecentTransaction />
+        </div>
       </div>
+
+      <Sidebar />
     </div>
   );
 };
